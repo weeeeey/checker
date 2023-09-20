37685ab0b220b53b48cec7cc70f7c38d33ab2295
zerojin <qser155@naver.com>
2023-09-20 19:03:19 +0900

    ##

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
 export default function App($app) {
     // Initialize your state here if needed
-    // const state = {
-    //     room: '',
-    //     currentUser: {
-    //         name: '',
-    //         cursor: 0,
-    //     },
-    //     members: [],
-    //     text: '',
+    this.state = {
+        isRoom: false,
+        currentUser: {
+            name: '',
+            cursor: 0,
+        },
+        members: [],
+        text: '',
+    };
+    const $room_make_btn = document.getElementById('room-make');
+    $room_make_btn.addEventListener('click', () => {
+        this.state.isRoom = true;
+
+        window.localStorage.setItem('room', JSON.stringify(this.state));
+    });
+    this.localStorage = JSON.parse(window.localStorage.getItem('room'));
+    console.log(this.localStorage);
+    // const $target = document.createElement('div');
+    // $target.className = 'Nickname';
+    // $app.append($target);
+
+    // this.render = () => {
+    //     $target.innerHTML = `
+    //         <h2>닉네임 설정</h2>
+    //         <hr style="width: 100%" />
+    //         <div class="Nickname-form">
+    //             <input
+    //                 id="nickname-input"
+    //                 type="text"
+    //                 placeholder="닉네임을 입력하세요."
+    //                 style="padding: 5px"
+    //             />
+    //             <button id="nickname-entry">입장</button>
+    //         </div>
+    //     `;
     // };
 
-    const $target = document.createElement('div');
-    $target.className = 'Nickname';
-    $app.append($target);
-
-    const render = () => {
-        $target.innerHTML = `
-            <h2>닉네임 설정</h2>
-            <hr style="width: 100%" />
-            <div class="Nickname-form">
-                <input
-                    id="nickname-input"
-                    type="text"
-                    placeholder="닉네임을 입력하세요."
-                    style="padding: 5px"
-                />
-                <button id="nickname-entry">입장</button>
-            </div>
-        `;
-    };
-
-    // Render the initial content
-    render();
-
-    return {
-        // You can expose any functions or properties here if needed
-        // getState: () => state,
-        // updateState: (newState) => { ... },
-    };
+    // // Render the initial content
+    // this.render();
 }
