c2047fc4fdd31f775c80c32cabad2250d22d86a8
zerojin <qser155@naver.com>
2023-09-23 16:09:25 +0900

    marker

--- ssrc/Room.js
+++ ssrc/Room.js
@@ -16,9 +16,21 @@ if (!members.some((member) => member.nickname === currntUser)) {
 
 const $editorContainer = document.querySelector('#editorContainer');
 const members_form = document.querySelector('.members-form');
-const cursor = document.createElement('div');
+const member_list = document.querySelector('.member-list');
+const member_alarm = document.querySelector('.member-alarm');
+
+const cursor_marker = document.createElement('span');
+cursor_marker.className = 'cursor-marker';
 
 $editorContainer.value = textareaData;
+member_alarm.innerHTML = `${
+    members[members.length - 1].nickname
+} 님이 입장했습니다.`;
+members.forEach((member) => {
+    const element = document.createElement('div');
+    element.innerHTML = member.nickname;
+    member_list.append(element);
+});
 
 window.addEventListener('storage', (e) => {
     if (e.key == 'room') {
@@ -27,7 +39,14 @@ window.addEventListener('storage', (e) => {
 
         if (newMembers.length != members.length) {
             const addedMember = newMembers[members.length];
-            members.append(addedMember);
+            members.push(addedMember);
+
+            const element = document.createElement('div');
+            element.innerHTML = addedMember.nickname;
+            member_list.append(element);
+
+            member_alarm.innerHTML = '';
+            member_alarm.innerHTML = `${addedMember.nickname} 님이 입장했습니다.`;
         } else {
             $editorContainer.value = newData.textareaData;
             textareaData = newData.textareaData;
@@ -43,5 +62,48 @@ $editorContainer.addEventListener('input', () => {
     window.localStorage.setItem('room', JSON.stringify(state));
 });
 
-$editorContainer.selectionStart = 10;
-$editorContainer.selectionEnd = 10;
+const createMarker = () => {
+    const marker = document.createElement('div');
+    marker.className = cursor - marker;
+    marker.textContent = currntUser;
+    return marker;
+};
+
+const getCursorXY = (input, selectionPoint) => {
+    const { offsetLeft: inputX, offsetTop: inputY } = input;
+    const div = document.createElement('div');
+    const copyStyle = getComputedStyle(input);
+    for (const prop of copyStyle) {
+        div.style[prop] = copyStyle[prop];
+    }
+    const swap = '.';
+    const inputValue =
+        input.tagName === 'INPUT'
+            ? input.value.replace(/ /g, swap)
+            : input.value;
+    // set the div content to that of the textarea up until selection
+    const textContent = inputValue.substr(0, selectionPoint);
+    // set the text content of the dummy element div
+    div.textContent = textContent;
+    if (input.tagName === 'TEXTAREA') div.style.height = 'auto';
+    // if a single line input then the div needs to be single line and not break out like a text area
+    if (input.tagName === 'INPUT') div.style.width = 'auto';
+    // create a marker element to obtain caret position
+    const span = document.createElement('span');
+    // give the span the textContent of remaining content so that the recreated dummy element is as close as possible
+    span.textContent = inputValue.substr(selectionPoint) || '.';
+    // append the span marker to the div
+    div.appendChild(span);
+    // append the dummy element to the body
+    document.body.appendChild(div);
+    // get the marker position, this is the caret position top and left relative to the input
+    const { offsetLeft: spanX, offsetTop: spanY } = span;
+    // lastly, remove that dummy element
+    // NOTE:: can comment this out for debugging purposes if you want to see where that span is rendered
+    document.body.removeChild(div);
+    // return an object with the x and y of the caret. account for input positioning so that you don't need to wrap the input
+    return {
+        x: inputX + spanX,
+        y: inputY + spanY,
+    };
+};
--- ssrc/index.js
+++ ssrc/index.js
@@ -11,6 +11,10 @@ let state = {
 };
 
 if (window.localStorage.getItem('room')) {
+    const temp = document.createElement('div');
+    temp.innerHTML = 'editor room이 존재합니다';
+    temp.style.color = 'red';
+    App.insertBefore(temp, App.firstChild);
     $roomBtn.style.display = 'none';
 } else {
     nickname.style.display = 'none';
@@ -25,14 +29,15 @@ $roomBtn.addEventListener('click', () => {
 const handleNicknameForm = (value) => {
     const $warn = document.querySelector('#warn');
     let localData = JSON.parse(window.localStorage.getItem('room'));
+    let membersNickname = localData.members.map((member) => member.nickname);
     if (value == '') {
         $warn.innerHTML = '닉네임을 입력하세요!';
-    } else if (localData.members.includes(value)) {
+    } else if (membersNickname.includes(value)) {
         $warn.innerHTML = '닉네임이 중복됩니다!';
     } else {
         localData = {
             ...localData,
-            members: [...localData.members, { nickname: value, x: 0, y: 0 }],
+            members: [...localData.members, { nickname: value, position: 0 }],
         };
         window.localStorage.setItem('room', JSON.stringify(localData));
         window.location.href =
